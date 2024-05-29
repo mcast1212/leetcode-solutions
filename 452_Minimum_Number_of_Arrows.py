c8dafdb6b4fb066f8c5f658c1e99be074fd1b737
mcast <michael.castaneda.1212@gmail.com>
2024-05-28 18:06:55 -0700

    first attempt

---
+++ 452_Minimum_Number_of_Arrows.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def findMinArrowShots(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        points.sort()
+        shots = 1
+        curr = points[0][1]
+        for point in points:
+            if point[0] > curr:
+                curr = point[1]
+                shots = shots + 1
+        return shots
