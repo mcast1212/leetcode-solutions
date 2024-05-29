fe3c2e5c421ab1c1a5861a6754753c708092109d
mcast <michael.castaneda.1212@gmail.com>
2024-05-28 18:38:25 -0700

    2nd attempt

--- 452_Minimum_Number_of_Arrows.py
+++ 452_Minimum_Number_of_Arrows.py
@@ -7,7 +7,18 @@ class Solution(object):
         points.sort()
         shots = 1
         curr = points[0][1]
+        extra = []
         for point in points:
+            if point[0] > curr:
+                curr = point[1]
+                shots = shots + 1
+            elif point[0] < curr and point[1] < curr:
+                extra.append(point)
+        if not extra:
+            return shots
+        shots = shots + 1
+        curr = extra[0][1]
+        for point in extra:
             if point[0] > curr:
                 curr = point[1]
                 shots = shots + 1
